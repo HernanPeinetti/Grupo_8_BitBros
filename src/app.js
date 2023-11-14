3a2cd7fed1f56f436a069ccdba13af5b1f4a6976
Lourdes Montenegro <147433147+lourdes-montenegro@users.noreply.github.com>
2023-11-14 13:05:56 +0000

    modificando carrito

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve("src/views", "index.html"));
 });
 
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.resolve("src/views", "productCart.html"));
+});
+
+
 app.listen(3000, () => {
     console.log("Servidor levantado en http://localhost:3000");
 });
