38e2d00bc5c5ab97de38da0533dcc218379263c1
GBonahora <r.gustavo.bonahora@gmail.com>
2023-11-15 02:48:44 -0300

    Añadidos: productDetail.html, productDetail.css, cons sus respectivos datos; algunas imágenes a modo de prueba. Este product detail es unicamente del producto inflador, más adelante, añadimos los restantes productos

--- src/app.js
+++ src/app.js
@@ -5,8 +5,12 @@ const app = express();
 app.use(express.static(path.resolve("src/public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("src/views", "index.html"));
-});
+    res.sendFile(path.resolve(__dirname,("./views/index.html")))
+})
+
+app.get("/producto", (req, res) => {
+    res.sendFile(path.resolve(__dirname,("./views/productDetail.html")))
+})
 
 app.listen(3000, () => {
     console.log("Servidor levantado en http://localhost:3000");
