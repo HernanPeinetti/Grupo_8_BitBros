5f20458e8d83f25c4a883fdfe9bce07def62694e
Hernan <hernan.peinetti18@gmail.com>
2023-11-15 02:59:24 -0300

    formulario de registro - faltan mejorar estilos y solucionar errores

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ app.use(express.static(path.resolve("src/public")));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("src/views", "index.html"));
 });
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve("src/views", "register.html"));
+});
 
 app.listen(3000, () => {
     console.log("Servidor levantado en http://localhost:3000");
