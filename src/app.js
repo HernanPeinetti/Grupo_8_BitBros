d9fcbd0b47365cf72ccf8de542bbd2d9dd074898
LeonelMatias199 <leonel.matias01@gmail.com>
2023-12-02 21:48:01 -0300

    avances en partials

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 
+//Configuraciones
 const app = express();
 app.use(express.static(path.resolve("src/public")));
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 //ROUTES 
 const mainRoutes = require('./routes/main.routes.js')
