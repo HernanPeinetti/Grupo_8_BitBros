3394761b2e1767b13309e66646f64d98fa978c87
MiltonC3 <miltoncoria03@gmail.com>
2023-12-02 22:19:41 -0300

    arreglado el partials y la ruta producto

--- src/app.js
+++ src/app.js
@@ -7,12 +7,12 @@ app.use(express.static(path.resolve("src/public")));
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 //ROUTES 
 const mainRoutes = require('./routes/main.routes.js')
 const productRoutes = require('./routes/product.routes.js')
 
-app.use('/', mainRoutes)
-app.use('/producto', productRoutes)
+app.use('/', mainRoutes, productRoutes)
 
 app.listen(3000, () => {
     console.log("Servidor levantado en http://localhost:3000");
