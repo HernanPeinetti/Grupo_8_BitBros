8195424774bb63f991666147da62f12b6b42cc13
Rocio <roespeche1@gmail.com>
2023-12-15 16:09:48 -0300

    comparti json de productos desde el controlador hacia la vista del index

--- src/controllers/main.controllers.js
+++ src/controllers/main.controllers.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const productos= require('../data/productsDataBase.json');
 
 const controllers = {
 
     index: (req, res) => {
-        res.render('index.ejs')
+        res.render('index.ejs', {productos : productos})
     },
 
     carrito: (req, res) => {
