bafe244ef280b2d69b2375421fdd0356ad902bce
GBonahora <r.gustavo.bonahora@gmail.com>
2023-12-19 04:30:15 -0300

    Agregado: users.json en data; configuraciones varias para el controlador de users y rutas. FALTA MEJORAR FUNCIONAMIENTO DEL JSON

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const multer = require("multer");
+const fs = require ("fs")
+const userPath = path.join(__dirname, "../data/users.json")
+const usuarios = JSON.stringify(fs.readFileSync(userPath, "utf-8"))
 
 const controllersUser = {
 
@@ -9,7 +13,20 @@ const controllersUser = {
     login: (req, res) => {
         res.render("./users/login.ejs");
     },
+
+    create: function(req, res){
+        let NuevoUsuario = {
+            ...req.bodys
+        }
+        usuarios.push(NuevoUsuario)
+
+        let usuarioJSON = JSON.stringify(usuarios, null, " ");
+        fs.writeFileSyncFileSync(userPath, usuarioJSON)
+        
+        res.redirect("./users/login.ejs")
+    }
     
 };
+
 
 module.exports = controllersUser;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const ruoter = express.Router();
+const multer = require("multer");
+const path = require("path")
 const controllers = require('../controllers/user.controllers.js');
 
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) =>{
+        cb(null, path.join(__dirname, "../data/user.json"))
+    },
+    filename: (req, res, cb) =>{
+        console.log(file)
+        const newFileName ="";
+        cb(null, );
+    } 
+})
+
+const upload = multer({storage})
 
 // VISTA LOGIN http://localhost:3000/login
 ruoter.get("/login", controllers.login)
 
 // VISTA REGISTER http://localhost:3000/register
 ruoter.get("/register", controllers.register)
+
+// REGISTER DE USUARIOS
+ruoter.post("/register", upload.single(), controllers.create)
+
+ruoter.post("/login", controllers.create)
 
 module.exports = ruoter; 
