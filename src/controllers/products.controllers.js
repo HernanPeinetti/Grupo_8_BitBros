35ad236f714677b9be6065e7caff1751e2f4c7be
Hernan <hernan.peinetti18@gmail.com>
2023-12-19 22:07:27 -0300

    avances update

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,9 +1,14 @@
 const path = require('path')
+const fs = require('fs');
+const productsFilePath = path.join(__dirname, '../data/products.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 
 const controllersProduct = {
 
     detalleProducto: (req, res) => {
         res.render('./products/productDetail.ejs')
+        
         
     },
 
@@ -14,6 +19,26 @@ const controllersProduct = {
     edit: (req, res) => {
         res.render('./market/edit.ejs')
     },
+
+    update: (req, res) =>{
+        const productsFilePath = path.join(__dirname, '../data/products.json');
+        let productoId = req.params.id;
+        const producto = products.find(producto => producto.id == productoId);
+
+        if(producto){
+
+
+
+        }
+
+
+    }
+        
+
+
+
+    
+
 
 }
 
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -8,9 +8,14 @@ ruoter.get("/detalle/:id", controllers.detalleProducto)
 
 
 // VISTA ITEM  http://localhost:3000/productos/editar/1
+//Edicion de producto
 ruoter.get("/editar/:id", controllers.edit)
+ruoter.put("/editar/:id", controllers.update);
 
 // VISTA CREATE http://localhost:3000/productos/crear
 ruoter.get("/crear", controllers.create)
+
+
+
 
 module.exports = ruoter; 
