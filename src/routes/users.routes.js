4e48be11bb0244600511497aa3d88f3dffb65d8f
Hernan <hernan.peinetti18@gmail.com>
2024-01-22 20:11:35 -0300

    validaciones register

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,51 @@
 const express = require('express')
+const fs = require('fs');
 const router = express.Router();
 const multer = require("multer");
 const path = require("path")
 const controllers = require('../controllers/users.controllers.js');
-<<<<<<< HEAD
+const userPath = path.join(__dirname, "../data/users.json");
 const {body} = require('express-validator');
 const validator = [
-    body('name').notEmpty().withMessage('Tienes que ingresar un nombre')
-];
-=======
-const pathImages = path.resolve("public")
->>>>>>> main
+    body('name').notEmpty().withMessage('Tienes que ingresar un nombre'),
+    body('birth').notEmpty().withMessage('Tienes que ingresar una fecha de nacimiento'),
+    body('email').notEmpty().withMessage('Tienes que ingresar un correo electronico').custom((value, {req}) =>{
+        let email = req.body.email;
+        const usuarios = JSON.parse(fs.readFileSync(userPath, "utf-8"))
+        const userFound = usuarios.find(user => user.email == email);
+        if(userFound){
+            throw new Error("El correo electronico ya esta en uso. Prueba uno nuevo"); 
+        }
+        return true;
 
+
+        
+    }),
+    body('password').notEmpty().withMessage('Tienes que ingresar una contraseña'),
+    body('avatar').custom((value, {req}) =>{
+        let file = req.file;
+        let extensions = [".jpg", ".png", ".gif"];
+
+        if (!file) {
+            throw new Error("Tienes que subir una imagen");
+        } else {
+            let fileExtension = path.extname(file.originalname);
+
+            if (!extensions.includes(fileExtension)) {
+                throw new Error(`Las extensiones de archivo permitidas son ${extensions.join(", ")}`);
+            }
+        }
+
+        return true;
+
+    })
+];
+
+
+
+
+
+const pathImages = path.resolve("public")
 const { login, register,create, processRegister} = require('../controllers/users.controllers.js')
 
 const storage = multer.diskStorage({
@@ -33,7 +67,8 @@ router.get("/login", login)
 router.get("/register", register)
 
 // REGISTER DE USUARIOS
-router.post("/register", upload.single("foto_perfil"), create)
 
-router.post('/register', processRegister)
+router.post("/register", upload.single("avatar"),validator, processRegister)
+
+
 module.exports = router; 
