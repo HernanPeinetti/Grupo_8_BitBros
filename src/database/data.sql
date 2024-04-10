2385ac449c52b06f6d86d6eeb3f2983a3abdb9e1
GBonahora <r.gustavo.bonahora@gmail.com>
2024-04-10 13:11:11 -0300

    Data editada

--- src/database/data.sql
+++ src/database/data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-04-2024 a las 17:13:17
+-- Tiempo de generación: 10-04-2024 a las 18:08:29
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `data`
 --
+CREATE DATABASE IF NOT EXISTS `data` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `data`;
 
 -- --------------------------------------------------------
 
